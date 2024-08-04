38dad23a83a3e6f608edbd56e717ec8429d8ff75
Nickjs101 <Nickjoshuamontemayor0121@gmail.com>
2024-08-04 12:21:31 +0800

    Contact and Clients

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -1,26 +1,130 @@
 import React from 'react';
+import depofagri from '../assets/Clients/Gov/depofagri.jpg'
+import dswd from '../assets/Clients/Gov/dswd.png'
+
+import dauagri from '../assets/Clients/Hardware/dauagri.jpg'
+import flowriteenterprise from '../assets/Clients/Hardware/flowriteenterprise.png'
+import inanama from '../assets/Clients/Hardware/inanama.jpg'
+import motionhardware from '../assets/Clients/Hardware/motionhardware.png'
+import nasajetmaticcenter from '../assets/Clients/Hardware/nasajetmaticcenter.jpg'
+import ncrconstructionsupply from '../assets/Clients/Hardware/ncrconstructionsupply.jpg'
+import newagoo from '../assets/Clients/Hardware/newagoo.jpg'
+import smagrimac from '../assets/Clients/Hardware/smagrimac.jpg'
+import superlakastrading from '../assets/Clients/Hardware/superlakastrading.jpg'
+import talico from '../assets/Clients/Hardware/talico.jpg'
+import twsteel from '../assets/Clients/Hardware/twsteel.jpg'
+import viganfortune from '../assets/Clients/Hardware/viganfortune.png'
+
+import palmacabanas from '../assets/Clients/Resort/palmcabanas.jpg'
+import poracayresort from '../assets/Clients/Resort/poracayresort.jpg'
+import villamartresort from '../assets/Clients/Resort/villamartresort.jpg'
+import wildorchid from '../assets/Clients/Resort/wildorchid.jpg'
+
+import deehwaliong from '../assets/Clients/School/deewhaliong.png'
+import dramandogarciamedcenter from '../assets/Clients/School/dramandogarciamedcenter.jpg'
+import obmontessoricenter from '../assets/Clients/School/obmontessoricenter.jpg'
+import stcatherine from '../assets/Clients/School/stcatherine.jpg'
+
+
+  const HardwareClients = [
+    { img: dauagri, name: 'Dau Agri Industrial Sales' },
+    { img: flowriteenterprise, name: 'Flow Rite Enterprise' },
+    { img: inanama, name: 'Inanama Realty Development Incorporated' },
+    { img: motionhardware, name: 'Motion Hardware' },
+    { img: nasajetmaticcenter, name: 'Nasa Jetmatic Center & General Services' },
+    { img: newagoo, name: 'New Agoo' },
+    { img: ncrconstructionsupply, name: 'NCR Construction Supply' },
+    { img: smagrimac, name: 'SM Agri-Mac' },
+    { img: superlakastrading, name: 'Super Lakas Trading' },
+    { img: talico, name: 'Talico' },
+    { img: twsteel, name: 'TW Steel' },
+    { img: viganfortune, name: 'Vigan Fortune Lumber & Hardware' },
+  ];
+  const Government = [
+    { img: depofagri, name: 'Department of Agriculture' },
+    { img: dswd, name: 'DSWD' },
+  ];
+
+  const Resort = [
+    { img: palmacabanas, name: 'Palm Cabanas' },
+    { img: poracayresort, name: 'Poracay Resort' },
+    { img: villamartresort, name: 'Villa Mart Resort' },
+    { img: wildorchid, name: 'Wild Orchid Resort' },
+  ];
+
+  const School = [
+    { img: deehwaliong, name: 'Dee Hwa Liong College Foundation' },
+    { img: obmontessoricenter, name: 'O.B. Montessori Center' },
+    { img: dramandogarciamedcenter, name: 'Dr. Amando L Garcia Medical Center Inc.' },
+    { img: stcatherine, name: 'St. Catherine of Alexandria Foundation and Medical Center' },
+  ];
 
 const Clients = () => {
   return (
     <section id="clients" className="py-16 bg-gray-100">
       <div className="container mx-auto">
-        <h2 className="text-3xl font-bold mb-4">Our Clients</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+        <h2 className="text-5xl font-bold mb-4 text-center md:text-left">Our Clients</h2>
+        <div className="flex flex-col gap-8">
           <div>
-            <h3 className="text-xl font-bold mb-2">Hardware & Construction Supply</h3>
-            {/* Add client details */}
+            <h3 className="text-xl font-bold mb-2 bg-gray-800 pl-4 text-white">Hardware & Construction Supply</h3>
+            <div className='bg-transparent text-gray-900 max-w-[1200px] grid grid-cols-3 place-items-center md:flex md:flex-wrap md:items-center relative z-10'>
+              {
+                HardwareClients.map((hwclient) => (
+                  <div key={hwclient.name} className='flex flex-col items-center m-4 sm:my-0 w-[70px] md:w-[100px]'>
+                    <div className='w-full h-[100px] flex items-center justify-center'>
+                      <img src={hwclient.img} alt={hwclient.name} className='w-full' />
+                    </div>
+                    <p className='mt-2 text-xs text-gray-400 font-medium font-sans text-center'>{hwclient.name}</p>
+                  </div>
+                ))
+              }
+            </div>
+            
           </div>
           <div>
-            <h3 className="text-xl font-bold mb-2">Villas & Resorts</h3>
-            {/* Add client details */}
+            <h3 className="text-xl font-bold mb-2 bg-blue-800 pl-4 text-white">Villas & Resorts</h3>
+            <div className='bg-transparent text-gray-900 max-w-[1200px] mx-auto grid grid-cols-3 place-items-center md:flex md:flex-wrap md:items-center relative z-10'>
+            {
+              Resort.map((resortclient) => (
+                <div key={resortclient.name} className='flex flex-col items-center m-4 sm:my-0 w-[70px] md:w-[100px]'>
+                  <div className='w-full h-[100px] flex items-center justify-center'>
+                    <img src={resortclient.img} alt={resortclient.name} className='max-h-full' />
+                  </div>
+                  <p className='mt-2 text-xs text-gray-400 font-medium font-sans text-center'>{resortclient.name}</p>
+                </div>
+              ))
+            }
+            </div>
           </div>
           <div>
-            <h3 className="text-xl font-bold mb-2">Government Facilities</h3>
-            {/* Add client details */}
+            <h3 className="text-xl font-bold mb-2 bg-red-800 pl-4 text-white">Government Facilities</h3>
+            <div className='bg-transparent text-gray-900 max-w-[1200px] mx-auto grid grid-cols-3 place-items-center md:flex md:flex-wrap md:items-center relative z-10'>
+              {
+                Government.map((govclient) => (
+                  <div key={govclient.name} className='flex flex-col items-center m-4 sm:my-0 w-[70px] md:w-[100px]'>
+                    <div className='w-full h-[100px] flex items-center justify-center'>
+                      <img src={govclient.img} alt={govclient.name} className='w-full' />
+                    </div>
+                    <p className='mt-2 text-xs text-gray-400 font-medium font-sans text-center'>{govclient.name}</p>
+                  </div>
+                ))
+              }
+            </div>
           </div>
           <div>
-            <h3 className="text-xl font-bold mb-2">Medical & Schools</h3>
-            {/* Add client details */}
+            <h3 className="text-xl font-bold mb-2 bg-green-800 pl-4 text-white">Medical & Schools</h3>
+            <div className='bg-transparent text-gray-900 max-w-[1200px] mx-auto grid grid-cols-3 place-items-center md:flex md:flex-wrap md:items-center relative z-10'>
+              {
+                School.map((schoolclient) => (
+                  <div key={schoolclient.name} className='flex flex-col items-center m-4 sm:my-0 w-[70px] md:w-[100px]'>
+                    <div className='w-full h-[100px] flex items-center justify-center'>
+                      <img src={schoolclient.img} alt={schoolclient.name} className='w-full' />
+                    </div>
+                    <p className='mt-2 text-xs text-gray-400 font-medium font-sans text-center'>{schoolclient.name}</p>
+                  </div>
+                ))
+              }
+            </div>
           </div>
         </div>
       </div>
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,47 +2,72 @@ import React from 'react';
 
 const Contact = () => {
   return (
-    <section id="contact" className="py-16">
-      <div className="container mx-auto">
-        <h2 className="text-3xl font-bold mb-4">Get in Touch</h2>
-        <div className="mb-4">
-          <h3 className="text-xl font-bold">Office Address</h3>
-          <p>7052 Legaspi ext. Duquit-Maisac Mabalacat City Pampanga</p>
-        </div>
-        <div className="mb-4">
-          <h3 className="text-xl font-bold">Factory Address</h3>
-          <p>Purok 7 Duquit Mabalacat City Pampanga</p>
-        </div>
-        <div className="mb-4">
-          <h3 className="text-xl font-bold">Phone</h3>
-          <p>0917-623-7040</p>
-        </div>
-        <div className="mb-4">
-          <h3 className="text-xl font-bold">Email</h3>
-          <p>cbuwtm@gmail.com</p>
-        </div>
-        <form className="bg-gray-100 p-4 rounded">
-          <div className="mb-4">
-            <label className="block mb-2">Name</label>
-            <input type="text" className="w-full p-2 border border-gray-300 rounded" />
+    <section id="contact" className="">
+      <div className="w-full h-auto md:h-auto lg:h-screen flex flex-col-reverse md:flex-row">
+        <iframe className='w-full md:w-1/2 h-auto' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d61608.61040422857!2d120.53052909565083!3d15.183688099609832!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3396ede876031c25%3A0x91be4da4dd790d2b!2sDuquit%2C%20Pampanga!5e0!3m2!1sen!2sph!4v1722735103353!5m2!1sen!2sph"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        
+        <div className='w-full md:w-1/2 p-4 md:p-12 lg:pr-36 '>
+          <h2 className="text-3xl font-bold font-serif mb-0">Get in Touch</h2> 
+          <p className='text-lg text-gray-400 mb-8'>Contact us for a quote</p>
+          <div className='flex flex-wrap gap-x-4 gap-y-0 mb-8 text-sm font-sans'>
+            <div className="mb-4 inline-block">
+              <h3 className="text-gray-400 text-xs">Office Address</h3>
+              <p>7052 Legaspi ext. Duquit-Maisac Mabalacat City Pampanga</p>
+            </div>
+            <div className="mb-4 inline-block">
+              <h3 className="text-gray-400 text-xs">Factory Address</h3>
+              <p>Purok 7 Duquit Mabalacat City Pampanga</p>
+            </div>
+            <div className="mb-4 inline-block">
+              <h3 className="text-gray-400 text-xs">Phone</h3>
+              <p>0917-623-7040</p>
+            </div>
+            <div className="mb-4 inline-block">
+              <h3 className="text-gray-400 text-xs">Email</h3>
+              <p>cbuwtm@gmail.com</p>
+            </div>
           </div>
-          <div className="mb-4">
-            <label className="block mb-2">Email</label>
-            <input type="email" className="w-full p-2 border border-gray-300 rounded" />
-          </div>
-          <div className="mb-4">
-            <label className="block mb-2">Phone</label>
-            <input type="text" className="w-full p-2 border border-gray-300 rounded" />
-          </div>
-          <div className="mb-4">
-            <label className="block mb-2">Message</label>
-            <textarea className="w-full p-2 border border-gray-300 rounded"></textarea>
-          </div>
-          <button type="submit" className="bg-blue-600 text-white px-4 py-2 rounded">Send Message</button>
-        </form>
-        {/* Add map embed here */}
+          
+
+          <form action='https://submit-form.com/OWbcYrEsl'>
+            <h2 className="text-xl font-bold font-serif mb-2">Get a quote</h2> 
+            <div className="mb-4">
+              <label className="block mb-2 text-gray-400 text-sm">Name</label>
+              <div className='inputdiv'>
+                <input className='contactinput' type="text" name='name' placeholder="Input Underline"/>
+                <span className='contactinputspan'></span>
+              </div>
+            </div>
+            <div className="mb-4">
+              <label className="block mb-2 text-gray-400 text-sm">Email</label>
+              <div className='inputdiv'>
+                <input className='contactinput' type="text" name='email' placeholder="Input Underline"/>
+                <span className='contactinputspan'></span>
+              </div>
+            </div>
+            <div className="mb-4">
+              <label className="block mb-2 text-gray-400 text-sm">Message</label>
+              <div className='textareadiv'>
+                <textarea className='tarea' type="text" name='message' placeholder="Input Trace"></textarea>
+                <span class="tareabottom tareaspan"></span>
+                <span class="tarearight tareaspan"></span>
+                <span class="tareatop tareaspan"></span>
+                <span class="tarealeft tareaspan"></span>
+              </div>
+            </div>
+            <div className='flex justify-end'>
+              <button type="submit" className=" hover:shadow-xl text-white bg-red-600 hover:bg-red-500 px-4 py-2 rounded">Send Message</button>
+            </div>
+          </form>
+
+          <h2 className="text-xl font-bold font-serif my-2">Follow us</h2> 
+
+        </div>
       </div>
     </section>
+
+
+
   );
 };
 
