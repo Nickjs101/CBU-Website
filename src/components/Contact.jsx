1e43a5423d264241bf98f912053b4cfe7e79bfaa
Nickjs101 <Nickjoshuamontemayor0121@gmail.com>
2024-08-04 12:30:33 +0800

    fix error in map

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,7 +4,7 @@ const Contact = () => {
   return (
     <section id="contact" className="">
       <div className="w-full h-auto md:h-auto lg:h-screen flex flex-col-reverse md:flex-row">
-        <iframe className='w-full md:w-1/2 h-auto' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d61608.61040422857!2d120.53052909565083!3d15.183688099609832!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3396ede876031c25%3A0x91be4da4dd790d2b!2sDuquit%2C%20Pampanga!5e0!3m2!1sen!2sph!4v1722735103353!5m2!1sen!2sph"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        <iframe title='CBUmap' className='w-full md:w-1/2 h-auto' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d61608.61040422857!2d120.53052909565083!3d15.183688099609832!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3396ede876031c25%3A0x91be4da4dd790d2b!2sDuquit%2C%20Pampanga!5e0!3m2!1sen!2sph!4v1722735103353!5m2!1sen!2sph"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         
         <div className='w-full md:w-1/2 p-4 md:p-12 lg:pr-36 '>
           <h2 className="text-3xl font-bold font-serif mb-0">Get in Touch</h2> 
