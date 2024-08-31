de5f2589935b6ff9e7acb1d75ae9c19f8e4e0c59
Nickjs101 <nickjoshuamontemayor0121@gmail.com>
2024-08-31 22:53:14 +0800

    fix team text

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
-import factorywarehouse from '../assets/OurTeam.jpg'
+import team from '../assets/OurTeam.jpg'
 const About = () => {
   return (
     <section className="bg-white">
-      <div  id="about" className="w-full mx-auto">
+      <div id="Team" className="w-full mx-auto">
             <div
-              className='w-full h-screen -mb-48 md:mb-0 text-neutral-800 flex gap-4 bg-contain md:bg-cover bg-no-repeat bg-center'
-              style={{ backgroundImage: `url(${factorywarehouse})` }}>
+              className='w-full h-auto lg:h-screen md:mb-0 text-neutral-800 flex gap-4 bg-contain md:bg-cover bg-no-repeat bg-center'
+              style={{ backgroundImage: `url(${team})` }}>
 
-              <div className='w-full h-full md:bg-gradient-to-b from-black/90 via-black/0 to-black/0 py-4 flex flex-col'>
+              <div className='w-full bg-neutral-50 lg:bg-transparent lg:bg-gradient-to-b from-black/90 via-black/0 to-black/0 py-4 flex flex-col'>
 
-                <div className='text-black md:text-white w-full text-center  mx-auto pt-4 md:pt-16'>
+                <div className='text-black lg:text-white w-full text-center  mx-auto pt-4 md:pt-16'>
                   <h2 className="article-h  font-bold mb-4">The Experts Behind Our Success</h2>
                   <p className='w-[400px] mb-4 contents article-p'>
                     Discover the talented individuals driving our innovation. Our team is a dynamic mix of seasoned professionals and creative problem-solvers with a passion for excellence that deliver top-quality water storage solutions, ensuring the highest standards in every product we create.
                   </p>
                 </div>
+                <img className='w-full h-auto mt-4 block lg:hidden px-4' src={team} alt="" />
                 
               </div>
               
