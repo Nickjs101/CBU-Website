f4f66dfe0fd09efb43c7669a3ab2d1deb40841be
Nickjs101 <nickjoshuamontemayor0121@gmail.com>
2024-09-01 21:09:19 +0800

    font and style

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,13 +4,35 @@ import logo from '../assets/Logo.png';
 
 const Home = ({gotoContact}) => {
   return (
+    // <section id="home" className="bg-cover bg-center h-screen text-neutral-100" style={{ backgroundImage: `url(${product1})` }}>
+    //   <div className="bg-black bg-opacity-50 h-full flex flex-col justify-center items-center">
+    //     <img src={logo} alt='Logo' className='absolute left-0 w-[500px] opacity-30' />
+    //     <h1 className="text-xl md:text-2xl lg:text-4xl md:w-10/12 font-bold mb-4 z-10 text-center">High-Quality Water Tanks for Every Need</h1>
+    //     <h1 className="text-4xl lg:text-7xl md:w-10/12 font-bold mb-4 z-10 text-center">Durable, Reliable, and Built to Last</h1>
+    //     <p className=" lg:text-xl mb-8 z-10 text-center">Discover a comprehensive selection of premium water tanks, engineered for residential, commercial, and industrial applications. Our tanks ensure reliable storage, exceptional durability, and superior performance.</p>
+    //     <a href="#products" onClick={(e) => gotoContact(e, 'contact')} className="bg-red-700 font-bold  px-10 py-3 z-10">GET A QUOTE</a>
+    //   </div>
+    // </section>
+
     <section id="home" className="bg-cover bg-center h-screen text-neutral-100" style={{ backgroundImage: `url(${product1})` }}>
-      <div className="bg-black bg-opacity-50 h-full flex flex-col justify-center items-center">
-        <img src={logo} alt='Logo' className='absolute left-0 w-[500px] opacity-30' />
-        <h1 className="text-xl md:text-2xl lg:text-4xl md:w-10/12 font-bold mb-4 z-10 text-center">High-Quality Water Tanks for Every Need</h1>
-        <h1 className="text-4xl lg:text-7xl md:w-10/12 font-bold mb-4 z-10 text-center">Durable, Reliable, and Built to Last</h1>
-        <p className=" lg:text-2xl mb-8 z-10 text-center">Discover a comprehensive selection of premium water tanks, engineered for residential, commercial, and industrial applications. Our tanks ensure reliable storage, exceptional durability, and superior performance.</p>
-        <a href="#products" onClick={(e) => gotoContact(e, 'contact')} className="border-2 font-bold hover:bg-neutral-100 hover:text-black px-6 py-3 z-10">GET A QUOTE</a>
+      <div className="bg-black bg-opacity-50 h-full grid grid-cols-1 lg:grid-cols-2 gap-8 items-center justify-center">
+        <div className='flex flex-col justify-evenly h-full p-8 md:p-20 lg:pr-0 lg:pl-20 xl:p-20 z-10'>
+          <div className=''>
+            <h1 className="text-2xl lg:text-4xl font-bold mb-4">High-Quality Water Tanks for Every Need</h1>
+            <h1 className="text-6xl lg:text-7xl  font-bold mb-4 z-10 ">Durable, Reliable, and Built to Last</h1>
+          </div>
+          <div className=''>
+            <p className=" lg:text-xl mb-8 z-10 ">Discover a comprehensive selection of premium water tanks, engineered for residential, commercial, and industrial applications. Our tanks ensure reliable storage, exceptional durability, and superior performance.</p>
+            <a href="#products" onClick={(e) => gotoContact(e, 'contact')} className="bg-red-700 font-bold px-10 py-3 z-10">GET A QUOTE</a>
+          </div>
+        </div>
+        <div className='absolute lg:relative h-full w-full flex justify-center items-center'>
+          <img src={logo} alt='Logo' className='w-[500px] lg:w-[600px] opacity-20 lg:opacity-40' />
+        </div>
+        
+        
+        
+        
       </div>
     </section>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,10 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+      oswald: ['Oswald', 'sans-serif'],
+    },},
   },
   plugins: [
   ],
